2df24cb920133455f4f92620a5a501bc371f509e
Sean Handley <sean.handley@gmail.com>
2014-04-08 09:31:05 +0100

    Set some mocks and expectations.

--- modules/dc_ldap/spec/functions/ldap_add_spec.rb
+++ modules/dc_ldap/spec/functions/ldap_add_spec.rb
@@ -4,10 +4,20 @@ require 'spec_helper'
 describe "ldapadd function" do
 	let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
+  before(:each) do
+    @ldap_entry = {username: 'testy_test', password: 'llama123',
+               first_name: 'Testy', last_name: 'Tester',
+               email: 'testy@test.com'}
+    @server_details = ["127.0.0.1", 8389, 'admin', 'Qqb8QC_wBeK!gch']
+    @params = @server_details.push(@ldap_entry)
+    host, port, username, password = @server_details
+    ldap_params = {host: host, username: username, password: password, port: port, method: :simple}
+    @mock_ldap = mock(bind: true)
+    @mock_ldap.expects(:add).returns(true)
+    Net::LDAP.expects(:new).with(ldap_params).returns(@mock_ldap)
+  end
+
 	it "should return hello" do
-    params = {username: 'testy_test', password: 'llama123',
-                           first_name: 'Testy', last_name: 'Tester',
-                           email: 'testy@test.com'}
-	  scope.function_ldap_add(["127.0.0.1", 8389, 'admin', 'Qqb8QC_wBeK!gch', params]).should == true
+	  scope.function_ldap_add(@params).should == true
 	end
 end
--- modules/dc_ldap/spec/spec_helper.rb
+++ modules/dc_ldap/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rspec-puppet'
 require 'rspec-puppet-utils'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'net/ldap'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
