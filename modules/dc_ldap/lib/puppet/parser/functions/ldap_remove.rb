fb6e0218935ce6ec3561ca2d20350843db1425b2
Sean Handley <sean.handley@gmail.com>
2014-04-08 17:26:58 +0100

    Add some heredoc.

--- modules/dc_ldap/lib/puppet/parser/functions/ldap_remove.rb
+++ modules/dc_ldap/lib/puppet/parser/functions/ldap_remove.rb
@@ -2,7 +2,17 @@ require_relative '../../../ldap_helper'
 require 'net/ldap'
 
 module Puppet::Parser::Functions
-  newfunction(:ldap_remove, :type => :rvalue) do |args|
+  newfunction(:ldap_remove, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+    Remove an entry in an LDAP directory.
+
+      Example:
+
+        # Remove entry for UID 'testy_test'.
+        ldap_remove(host, port, admin_user, admin_password,
+          :dn => 'uid=testy_test,ou=People,dc=example,dc=net')
+
+    ENDHEREDOC
+
     extend LDAPHelper
     ldap = ldap(args)
     ldap.remove(params(args))
--- modules/dc_ldap/lib/puppet/parser/functions/ldap_search.rb
+++ modules/dc_ldap/lib/puppet/parser/functions/ldap_search.rb
@@ -2,7 +2,18 @@ require_relative '../../../ldap_helper'
 require 'net/ldap'
 
 module Puppet::Parser::Functions
-  newfunction(:ldap_search, :type => :rvalue) do |args|
+  newfunction(:ldap_search, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+    Search for an entry in an LDAP directory.
+
+      Example:
+
+        # Search in the example.net base for a mail with 'a*.com' matching.
+        ldap_search(host, port, admin_user, admin_password, 
+                   :base => 'dc=example, dc=net', :filter => ['mail', 'a*.com'],
+                   :attributes => ["mail", "cn", "sn", "objectclass"])
+
+    ENDHEREDOC
+
     extend LDAPHelper
     ldap = ldap(args)
     ldap.search(params(args).merge(filter: Net::LDAP::Filter.eq(*params(args)[:filter])),
